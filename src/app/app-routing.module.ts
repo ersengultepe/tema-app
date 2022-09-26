1e6fa2bca4afc7108d71af3916a2d4830e5e0749
tanersaydam <tanersaydam@gmail.com>
2022-09-26 14:29:37 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './components/blank/blank.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutsComponent,
+    children: [
+      {
+        path: "",
+        component: BlankComponent 
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { MainSidebarComponent } from './components/layouts/main-sidebar/main-sidebar.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { ControlSidebarComponent } from './components/layouts/control-sidebar/control-sidebar.component';
+import { BlankComponent } from './components/blank/blank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutsComponent,
+    NavbarComponent,
+    MainSidebarComponent,
+    FooterComponent,
+    ControlSidebarComponent,
+    BlankComponent
   ],
   imports: [
     BrowserModule,
