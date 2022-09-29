685b2af8b13ecba36b1bb4f0f07f7746b173d861
tanersaydam <tanersaydam@gmail.com>
2022-09-29 18:37:24 +0300

    .

--- src/app/components/personel/personel.component.ts
+++ src/app/components/personel/personel.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { BolumModel } from '../bolum/models/bolum.model';
+import { PersonelModel } from './models/personel.model';
 
 @Component({
   selector: 'app-personel',
@@ -11,19 +13,57 @@ import { BolumModel } from '../bolum/models/bolum.model';
 export class PersonelComponent implements OnInit {
 
   bolumler: BolumModel[] = [];
+  personels: PersonelModel[] = [];
   constructor(
     private _http: HttpClient
   ) { }
 
   ngOnInit(): void {
     this.bolumGetList();
+    this.getList();
   }
 
-  bolumGetList(){
+  bolumGetList(){   
     let api = environment.api + "PersonelBolums/GetList";
     this._http.get<any>(api).subscribe({
       next: (res)=> {
         this.bolumler = res.data;
+      },
+      error: (err)=> console.log(err)
+    })
+  }
+
+  kaydet(form: NgForm){
+    if (!form.valid) {
+      return;
+    }
+
+    let api = environment.api + "Personels/Add";
+    this._http.post<any>(api, form.value).subscribe({
+      next: (res)=> {
+        form.reset();
+        this.getList();
+      },
+      error: (err)=> console.log(err)
+    })
+  }
+
+  getList(){
+    let api = environment.api + "Personels/GetList";
+    this._http.get<any>(api).subscribe({
+      next: (res)=> {
+        this.personels = res.data;
+      },
+      error: (err)=> console.log(err)
+    })
+  }
+
+  delete(model: PersonelModel){
+    let api = environment.api + "Personels/Delete";
+    this._http.post<any>(api, model).subscribe({
+      next: (res)=> {
+        //form.reset();
+        this.getList();
       },
       error: (err)=> console.log(err)
     })
