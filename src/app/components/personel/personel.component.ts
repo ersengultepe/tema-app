81a1940ed7d913d54932b63968b4017b3aa14094
tanersaydam <tanersaydam@gmail.com>
2022-09-29 14:48:44 +0300

    .

--- src/app/components/personel/personel.component.ts
+++ src/app/components/personel/personel.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BolumModel } from '../bolum/models/bolum.model';
 
 @Component({
   selector: 'app-personel',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonelComponent implements OnInit {
 
-  constructor() { }
+  bolumler: BolumModel[] = [];
+  constructor(
+    private _http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  bolumGetList(){
+    let api = environment.api + "PersonelBolums/GetList";
+    this._http.get<any>(api).subscribe({
+      next: (res)=> {
+        this.bolumler = res.data;
+      },
+      error: (err)=> console.log(err)
+    })
   }
 
 }
